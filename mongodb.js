c79ba466d8b6f54fcdd9150a16794e80053078ff
Allison <Allison.Boone@keyin.com>
2024-12-02 17:34:06 -0330

    Edited minor typos and syntax errors, works as expected

--- mongodb.js
+++ mongodb.js
@@ -6,45 +6,29 @@ const client = new MongoClient(uri);
 (async () => {
   try {
     await client.connect();
-    console.log('Connect to MongoDB');
+    console.log('Connected to MongoDB');
 
     const db = client.db('library');
     const collection = db.collection('books');
 
     await collection.insertMany([
       {
-        title: 'The Silent Patient',
-        author: 'Alex Michaelides',
-        genre: 'Psychological Thriller',
-        year: 2019,
+        title: 'The Hobbit',
+        author: 'J.R.R. Tolkien',
+        genre: 'Fantasy',
+        year: 1937,
       },
-
       {
-        title: 'Project Hail Mary',
-        author: 'Andy Weir',
-        genre: 'Science Fiction',
-        year: 2021,
+        title: 'To Kill a Mockingbird',
+        author: 'Harper Lee',
+        genre: 'Fiction',
+        year: 1960,
       },
-
       {
-        title: 'Where the Crawdads Sing',
-        author: 'Delia Owens',
-        genre: 'Literary Fiction',
-        year: 2018,
-      },
-
-      {
-        title: 'Atomic Habits',
-        author: 'James Clear',
-        genre: 'Self-Help',
-        year: 2018,
-      },
-
-      {
-        title: 'Circe',
-        author: 'Madeline Miller',
-        genre: 'Mythological Fantasy',
-        year: 2018,
+        title: '1984',
+        author: 'George Orwell',
+        genre: 'Dystopian',
+        year: 1949,
       },
     ]);
     console.log('Sample data inserted');
@@ -52,19 +36,21 @@ const client = new MongoClient(uri);
     const titles = await collection
       .find({}, { projection: { title: 1, _id: 0 } })
       .toArray();
-    console.log('Title of all books: ', titles);
+    console.log('Titles of all books:', titles);
 
-    const andyBooks = await collection.find({ author: 'Andy Weir' }).toArray();
-    console.log('Books written by Andy Weir: ', andyBooks);
+    const tolkienBooks = await collection
+      .find({ author: 'J.R.R. Tolkien' })
+      .toArray();
+    console.log('Books written by J.R.R. Tolkien:', tolkienBooks);
 
     await collection.updateOne(
-      { title: 'Circe' },
-      { $set: { genre: 'Fantasy' } }
+      { title: '1984' },
+      { $set: { genre: 'Science Fiction' } }
     );
-    console.log('Updated genre of Circe to "Fantasy"');
+    console.log('Updated genre of "1984" to "Science Fiction"');
 
-    await collection.deleteOne({ title: 'Where the Crawdads Sing' });
-    console.log('"Where the Crawdads Sing" has been deleted');
+    await collection.deleteOne({ title: 'The Hobbit' });
+    console.log('"The Hobbit" has been deleted');
   } catch (error) {
     console.error('Error:', error);
   } finally {
